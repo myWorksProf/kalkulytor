a1b0e9ee0557c0d42d095b97fd47fc6a3c1d3b86
myWorksProf <spovsem@gmail.com>
2022-05-10 22:32:02 +0700

    завершенный калькулятора

--- src/dop_homeWork/kalkulytor/src/main.java
+++ src/dop_homeWork/kalkulytor/src/main.java
@@ -1,22 +1,21 @@
 import java.util.Scanner;
 
 import static java.lang.System.*;
-
-public class kalkulytor {
+public class main {
 
     public static void main(String[] args) {
-        out.println("Введите операцию:");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первое число = ");
+        int a = scanner.nextInt();
+        System.out.print("Введите второе число = ");
+        int b = scanner.nextInt();
         out.println("1. Сложение ( + )");
         out.println("2. Вычитание ( - )");
         out.println("3. Умножение ( * )");
         out.println("4. Деление ( / )");
         out.println("5. Остаток от деления ( % )");
-        Scanner scanner = new Scanner(System.in);
+        out.print("Введите ноиер операции = ");
         int operation = scanner.nextInt();
-        System.out.print("Введите первое число = ");
-        int a = scanner.nextInt();
-        System.out.print("Введите второе число = ");
-        int b = scanner.nextInt();
         int result;
         if (operation == 1) {
             result = a + b;
