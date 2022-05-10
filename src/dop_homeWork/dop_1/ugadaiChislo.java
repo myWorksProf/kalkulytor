999f015a777cb3105553bb8690a12e5dd667ddd3
myWorksProf <spovsem@gmail.com>
2022-05-10 19:35:21 +0700

    первая проба калькулятора

--- src/dop_homeWork/dop_1/ugadaiChislo.java
+++ src/dop_homeWork/dop_1/ugadaiChislo.java
@@ -1,7 +1,13 @@
 package dop_homeWork.dop_1;
 
+import java.util.Scanner;
+
 public class ugadaiChislo {
     public static void main(String[] args) {
-
+        System.out.println("Угадай число!");
+        System.out.println("Введите число ");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println("a = " + a);
     }
 }
